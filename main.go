6c7a40c9b382c9d60e96d5125c083bff2f5bef46
Johan Sundell <johan@pixpro.net>
2018-04-16 14:56:56 +0200

    Added the code for the service

---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"bytes"
+	"encoding/base64"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/tuotoo/qrcode"
+)
+
+func main() {
+	intf := flag.String("interface", ":8080", "The networkinterface and port to run on")
+
+	http.HandleFunc("/", decodeBin)
+	//http.HandleFunc("/base64", decodeBase64)
+	log.Println(http.ListenAndServe(*intf, nil))
+}
+
+func testFunc() {
+	/*fi, err := os.Open("test.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fi.Close()
+	info, err := readImage(fi)
+	log.Println(info)*/
+}
+
+func decodeBase64(w http.ResponseWriter, r *http.Request) {
+	str64 := r.URL.Query().Get("qr")
+	b, err := base64.StdEncoding.DecodeString(str64)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fi := bytes.NewReader(b)
+	str, err := readImage(fi)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintf(w, str)
+
+}
+
+func decodeBin(w http.ResponseWriter, r *http.Request) {
+	log.Println("Start")
+	err := r.ParseMultipartForm(100000)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	for _, files := range r.MultipartForm.File {
+		for _, file := range files {
+			log.Println(file.Filename)
+			f, err := file.Open()
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			defer f.Close()
+			str, err := readImage(f)
+			if err != nil {
+				log.Println(err)
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			fmt.Fprintf(w, str+"\n")
+		}
+	}
+}
+
+func readImage(fi io.Reader) (string, error) {
+	qrinfo, err := qrcode.Decode(fi)
+	if err != nil {
+		return "", err
+	}
+	return qrinfo.Content, nil
+
+}
